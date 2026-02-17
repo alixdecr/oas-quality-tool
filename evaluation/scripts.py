ca12ef03cdd268ff0f906170023cad5445df1627
alixdecr <alix.decrop@unamur.be>
2026-02-17 13:38:36 +0100

    update: charts

--- evaluation/scripts.py
+++ evaluation/scripts.py
@@ -81,8 +81,6 @@ print(f"Very Large: > {p95}")
 # ---------------------
 # QUALITY SCATTER PLOT
 # ---------------------
-index = 0
-api_ids = []
 qualities = []
 colors = []
 routes = []
@@ -91,7 +89,6 @@ for json_path in OUTPUTS_PATH.glob("*.json"):
     with open(json_path, "r", encoding="utf-8-sig") as file:
         data = json.load(file)
 
-    index += 1
     nb_routes = data["structure"]["routes"]["total"]
     quality = data["quality"]["normalized"]
     color = "tab:blue"
@@ -107,7 +104,6 @@ for json_path in OUTPUTS_PATH.glob("*.json"):
     else:
         color = "tab:red"
 
-    api_ids.append(index)
     qualities.append(quality)
     colors.append(color)
     routes.append(nb_routes)
@@ -145,7 +141,7 @@ plt.close()
 
 
 # ------------------
-# EVALUATIONS GRAPH
+# EVALUATIONS CHART
 # ------------------
 evaluations = {}
 
@@ -179,3 +175,67 @@ ax.grid(axis="x", linestyle="--")
 plt.tight_layout()
 plt.savefig(CHARTS_PATH / "chart-evaluations-count.pdf", format="pdf")
 plt.close()
+
+
+# ----------------------------------
+# QUALITY SCATTER PLOT BY DIMENSION
+# ----------------------------------
+for dimension in ["format", "oas-version", "metadata", "server", "descriptions", "examples"]:
+
+    qualities = []
+    colors = []
+    routes = []
+
+    for json_path in OUTPUTS_PATH.glob("*.json"):
+        with open(json_path, "r", encoding="utf-8-sig") as file:
+            data = json.load(file)
+
+        nb_routes = data["structure"]["routes"]["total"]
+        quality = data["evaluation-groups"][dimension]["pass"] / data["evaluation-groups"][dimension]["total"]
+        color = "tab:blue"
+
+        if nb_routes <= p5:
+            color = "tab:blue"
+        elif nb_routes <= p35:
+            color = "tab:green"
+        elif nb_routes <= p65:
+            color = "tab:olive"
+        elif nb_routes <= p95:
+            color = "tab:orange"
+        else:
+            color = "tab:red"
+
+        qualities.append(quality)
+        colors.append(color)
+        routes.append(nb_routes)
+
+    points = list(zip(routes, qualities))
+    counts = Counter(points)
+    sizes = [counts[(xi, yi)] * 100 for xi, yi in points]
+
+    plt.figure(figsize=(20, 12))
+    plt.scatter(routes, qualities, c=colors, s=sizes)
+
+    plt.xscale("log")
+    plt.xlabel("API Size", fontweight="bold", fontsize=16, labelpad=20)
+    plt.ylabel(f"Quality of {dimension}", fontweight="bold", fontsize=16, labelpad=20)
+
+    legend_elements = [
+        Patch(facecolor="tab:blue", label=f"Micro (≤ {p5} routes)"),
+        Patch(facecolor="tab:green", label=f"Small ({p5}-{p35} routes)"),
+        Patch(facecolor="tab:olive", label=f"Medium ({p35 + 1}-{p65} routes)"),
+        Patch(facecolor="tab:orange", label=f"Large ({p65 + 1}-{p95} routes)"),
+        Patch(facecolor="tab:red", label=f"Very large (> {p95} routes)"),
+    ]
+
+    plt.legend(
+        handles=legend_elements,
+        title="API Size Based on Number of Routes",
+        title_fontsize=15,
+        fontsize=14,
+        loc="upper right"
+    )
+
+    plt.tight_layout()
+    plt.savefig(CHARTS_PATH / f"chart-quality-dimension-{dimension}.pdf", format="pdf")
+    plt.close()
